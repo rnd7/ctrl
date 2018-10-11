536ac84e8a8022253fe7cad257bc51d4bad40bcf
Nicholas Schreiber <rnd7@riseup.net>
2018-10-11 10:08:35 +0200

    Changed build procedure.

--- tasks/ctrl.js
+++ tasks/ctrl.js
@@ -7,19 +7,49 @@ import sourcemaps from 'gulp-sourcemaps'
 import concat from 'gulp-concat'
 import rename from 'gulp-rename'
 import gulpIgnore from 'gulp-ignore'
+import webpack from 'webpack'
+import webpackStream from 'webpack-stream'
 
-const isProduction = (process.env.NODE_ENV === 'production')
+const isProduction = true // (process.env.NODE_ENV === 'production')
+
+
+export const config = {
+  entry: './index.js',
+  mode: isProduction ? "production" : "development",
+  output: {
+    filename: 'ctrl-min.js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  context: path.resolve(__dirname, '../src/ctrl'),
+  optimization: {
+    minimize: isProduction ? true : false
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [ {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }]
+  },
+  plugins: [],
+  resolve: {
+    extensions: ['.js'],
+    modules: ['node_modules']
+  }
+}
 
 export const bundleComponents = () => {
-  return gulp.src('./src/ctrl/index.js')
-    .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['@babel/preset-env']
-    }))
-    .pipe(uglify())
-    .pipe(rename({basename:"ctrl-min"}))
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./dist'))
+  return new Promise(resolve => webpack(config, (err, stats) => {
+    if(err) console.log('Webpack', err)
+    console.log(stats.toString({ }))
+    resolve()
+  }))
 }
 
 export const transpileComponents = () => {
